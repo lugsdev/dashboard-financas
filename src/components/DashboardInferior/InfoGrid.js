070e1822063b7422623ba89ec81ef185aa9e8107
Lucas Almeida <113302691+lugsdev@users.noreply.github.com>
2023-02-24 16:22:35 -0300

    alteração da função infoGrid

--- src/components/DashboardInferior/InfoGrid.js
+++ src/components/DashboardInferior/InfoGrid.js
@@ -1,57 +1,34 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import { DataGrid } from '@mui/x-data-grid';
+
 import styles from "./InfoGrid.module.css"
 
-const columns = [
-  { field: 'id',
-    headerName: "Descritivo",
-    width: 240, 
-    sortable: false
-  },
-  {
-    field: 'valor',
-    headerName: "Valor",
-    width: 120,
-    editable: true,
-    sortable: false,
-  },
-  {
-    field: 'porcentagem',
-    headerName: "%",
-    width: 70,
-    editable: false,
-  }
-];
+const data = [
+  {nome: "Total receita", valor: "4.190,00", porcento: "100"},
+  {nome: "Custo de bens vendidos", valor: "1.417,00", porcento: "34"},
+  {nome: "Lucro bruto", valor: "2.773,00", porcento: "66"},
+  {nome: "Total Despesas Operacionais", valor: "1.961,00", porcento: "47"},
+  {nome: "Lucro Operacional", valor: "812,00", porcento: "19"},
+  {nome: "Imposto", valor: "520,00", porcento: "12"},
+  {nome: "Lucro Líquido", valor: "292,00", porcento: "7"}
+]
 
-const rows = [
-  { id: "Total Receita", valor: "4.190,00", porcentagem: '100%'},
-  { id: "Custo de bens vendidos", valor: '1.417,00', porcentagem: '34%'},
-  { id: "Lucro bruto", valor: '2.773,00', porcentagem: '66%'},
-  { id: "Total Despesas Operacionais", valor: '1.961,00', porcentagem: '47%'},
-  { id: "Lucro operacional", valor: '812,00', porcentagem: '19%'},
-  { id: "Imposto", valor: '520,00', porcentagem: "12%"},
-  { id: "Lucro Líquido", valor: '292,00', porcentagem: '7%'},
-];
+function InfoGrid(){
+  return(
+    <div className={styles.InfoGrid}>
+      <h2>Demonstração de Resultados</h2>
 
-function InfoGrid() {
-  return (
-
-    
-      <div className={styles.InfoGrid}>
-        <h2>Demonstração de resultados</h2>
-      
-      <Box sx={{ height: 290, width: 450 }}>
-        <DataGrid
-          rows={rows}
-          columns={columns}
-          pageSize={7}
-          rowHeight={27}
-          disableSelectionOnClick
-        />
-      </Box>
-      </div>
-    );
-}
+      <table>
+        {data.map((val, key) => {
+          return (
+            <tr key={key}>
+              <td>{val.nome}</td>
+              <td>{val.valor}</td>
+              <td>{val.porcento + "%"}</td>
+            </tr>
+          )
+        })}
+      </table>
+    </div>
+  )
+};
 
 export default InfoGrid
